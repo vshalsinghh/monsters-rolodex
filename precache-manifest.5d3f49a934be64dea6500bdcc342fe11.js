7d7d074e6f877a20664906327dd4261b8ebb2390
xCvishal <vishalsingh2402@gmail.com>
2020-10-10 20:40:10 +0530

    Updates

---
+++ precache-manifest.5d3f49a934be64dea6500bdcc342fe11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03ddd9d366b0b603b1f5f2465eabf52",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ac2387d82b426a04846d",
+    "url": "/monsters-rolodex/static/css/main.8f7fec08.chunk.css"
+  },
+  {
+    "revision": "3efe442043e6563e003d",
+    "url": "/monsters-rolodex/static/js/2.4c70bcfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.4c70bcfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2387d82b426a04846d",
+    "url": "/monsters-rolodex/static/js/main.c2d20c38.chunk.js"
+  },
+  {
+    "revision": "175dbf2b726cf515ab05",
+    "url": "/monsters-rolodex/static/js/runtime-main.6c9e3d0a.js"
+  }
+]);
